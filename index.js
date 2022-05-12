ad1c9f57be4b20ffef16b4dc06865b52e53a89d2
Patrick Young <patrickyoung281@gmail.com>
2022-05-12 16:50:41 -0400

    commit tree b068c4aa832448472ef5f7be988f213b8db78d66

--- index.js
+++ index.js
@@ -1,1 +1,119 @@
-// Code your solution in this file!
+
+
+function sayHello () {
+  return "Hello!"
+}
+
+function receivesAFunction (callBack) {
+  return callBack();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+
+function returnFirstTwoDrivers (drivers) {
+  return drivers.slice (0, 2);
+};
+
+function returnLastTwoDrivers (drivers) {
+  return drivers.slice (2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (fareMultiplier) {
+  return function (value) {
+    return fareMultiplier*value};
+};
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers (drivers, whichDrivers) {
+  return whichDrivers(drivers);
+};
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2)
+};
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(2)
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]; 
+
+const createFareMultiplier = function (fareMultiplier) {
+    return function(value) {
+      return fareMultiplier * value;
+    }
+  }
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const fareQuintupler = createFareMultiplier(4);
+
+const selectDifferentDrivers = function (drivers, whichDrivers) {
+    return whichDrivers(drivers)};
+
+    */
